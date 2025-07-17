bff8c0ed95eadbf742c1ba9202cf980a80a74d4b
Maksym Khavil <mk.khavil@gmail.com>
2025-07-17 13:19:14 +0200

    ft: add constants script

---
+++ marketing_campaign_data/R/constants.R
@@ -0,0 +1,21 @@
+
+COLORS = c(
+    age = "#fcba03",
+    job = "#a5fc03",
+    marital = "#03fc0b",
+    education = "#03fcdb",
+    default = "#03d3fc",
+    balance = "#035efc",
+    housing = "#8403fc",
+    loan = "#fc03f0",
+    contact = "#fc0324",
+    day = "#692208",
+    month = "#526908",
+    duration = "#08695e",
+    campaign = "#084269",
+    pdays = "#350869",
+    previous = "#c4f7ff",
+    poutcome = "#fff4c4",
+    
+    y = "#aaaaaa"
+)
